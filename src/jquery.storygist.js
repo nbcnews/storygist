c0232a9ebf2ee709f5268e6087bf2abc5a60a129
EJ Fox <ejfox@ejfox.com>
2017-05-19 11:23:17 -0400

    removes old unused code

--- src/jquery.storygist.js
+++ src/jquery.storygist.js
@@ -142,10 +142,6 @@
       // Find the original element that corresponds with the current beat
       var scrollToEl = $(plugin.settings.contentParent + ' ' + plugin.settings.beatSelector + ':eq(' + currentBeatNum + ')')
 
-      // Get that element's offset
-      // var scrollOffset = scrollToEl.offset().top
-      // scrollToEl.css('border', '5px solid red')
-
       // Scroll to that element
       $('html, body').animate({
         scrollTop: (scrollToEl.offset().top - 80)
