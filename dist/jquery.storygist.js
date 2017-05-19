55d0e234c5d15b6794902fba5d90c87c72c24c7d
EJ Fox <ejfox@ejfox.com>
2017-05-19 16:40:33 -0400

    add random transition on pullquote, blur-in on videos

--- dist/jquery.storygist.js
+++ dist/jquery.storygist.js
@@ -78,6 +78,9 @@
     }
 
     plugin.nextBeat = function (beatNum, el) {
+      function getRandTransition () {
+        return transitions[Math.floor(Math.random() * transitions.length)]
+      }
       // Handle behavior to move to next beat
       // A click on the right side of the window
       if ($(el).hasClass('last')) {
@@ -88,6 +91,18 @@
 
         $nextEl.addClass('active')
 
+        var baseAnimSpeed = 750
+        var blurPx = 81
+
+        var transitions = ['transition.slideLeftIn',
+          'transition.slideDownIn',
+          'transition.slideLeftBigIn',
+          'transition.shrinkIn',
+          'transition.flipXIn',
+          'transition.flipYIn',
+          'transition.fadeIn',
+          'transition.expandIn']
+
         if ($nextEl.find('p').length) {
           console.log('HAS A P TAG')
           var split = new SplitType($nextEl.find('p'), {
@@ -96,13 +111,31 @@
           })
           console.log('split', split)
           $nextEl.find('.line')
-          .velocity('transition.shrinkIn', {'duration': 800, 'stagger': 18})
+          .velocity('transition.shrinkIn', {'duration': baseAnimSpeed * 0.6, 'stagger': baseAnimSpeed * 0.05})
         }
 
-        $nextEl.find('figure').velocity({'blur': 0},
-          { 'duration': 1000,
+        $nextEl.find('figure img')
+        // .css('margin-bottom', -800)
+        .velocity({
+          'blur': 0
+          // 'margin-bottom': 0,
+        },
+          { 'duration': baseAnimSpeed,
             'begin': function (el) {
-              $(el).css('-webkit-filter', 'blur(91px)')
+              $nextEl.find('figure figcaption').css('opacity', 0)
+              $(el).css('-webkit-filter', 'blur(' + blurPx + 'px)')
+            },
+            'complete': function (el) {
+              $nextEl.find('figure figcaption')
+              .velocity('transition.slideLeftIn', {'duration': baseAnimSpeed * 1.75})
+            }
+          })
+
+        $nextEl.find('figure.media')
+        .velocity({ 'blur': 0 },
+          { 'duration': baseAnimSpeed,
+            'begin': function (el) {
+              $(el).css('-webkit-filter', 'blur(' + blurPx + 'px)')
             }
           })
 
@@ -112,7 +145,7 @@
             split: 'lines'
           })
           $nextEl.find('.line')
-          .velocity('transition.slideLeftIn', {'duration': 800, 'stagger': 400})
+          .velocity(getRandTransition(), {'duration': baseAnimSpeed, 'stagger': baseAnimSpeed / 2})
 
           /*
           var fontSize = $nextEl.find('.pullquote').css('font-size')
@@ -182,10 +215,6 @@
 
       // Find the original element that corresponds with the current beat
       var scrollToEl = $(plugin.settings.contentParent + ' ' + plugin.settings.beatSelector + ':eq(' + currentBeatNum + ')')
-
-      // Get that element's offset
-      // var scrollOffset = scrollToEl.offset().top
-      // scrollToEl.css('border', '5px solid red')
 
       // Scroll to that element
       $('html, body').animate({
