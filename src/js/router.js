b1f221f7fdcea69eb22315f77b23ce176010128d
brad oyler <bradoyler@gmail.com>
2017-07-12 11:29:43 -0400

    extract Navigo and load as global dependency (#58)
    * extract Navigo and load as global dependency
    
    * cleanup options
    
    * remove unused file

--- src/js/router.js
+++
@@ -1,9 +0,0 @@
-/* globals jQuery, StoryGist */
-// router.js
-// NOT USED, trying out Navigo instead...
-;(function ($, sg) {
-  sg.Router = {}
-  sg.Router.config = function (options) {}
-  sg.Router.navigate = function (path) {}
-  sg.Router.on = function (route, callback) {}
-})(jQuery, StoryGist)
