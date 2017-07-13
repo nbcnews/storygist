f98bbc5e3fa9f4ac75420e9f09e4bf36238d2d31
EJ Fox <ejfox@ejfox.com>
2017-07-13 16:21:22 -0400

    add beatNums to prev and next events

--- src/js/events.js
+++ src/js/events.js
@@ -53,7 +53,7 @@ function nextBeat (beatNum, el) {
   // A click on the right side of the window
   debug('>> Next', beatNum)
 
-  Tracking.sendEvent('nextBeat')
+  Tracking.sendEvent('nextBeat', beatNum)
   pauseBeats()
 
   if ($(el).hasClass('last')) {
@@ -118,7 +118,7 @@ function nextBeat (beatNum, el) {
 }
 
 function prevBeat (beatNum, el) {
-  Tracking.sendEvent('prevBeat')
+  Tracking.sendEvent('prevBeat', beatNum)
 
   debug('>> Prev', beatNum)
   pauseBeats()
