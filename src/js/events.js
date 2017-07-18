f865e58c031b35ff70773bd3d81172174c20ef6a
EJ Fox <ejfox@ejfox.com>
2017-07-18 11:16:20 -0400

    brad magic

--- src/js/events.js
+++ src/js/events.js
@@ -218,7 +218,7 @@ function clickBeat (e) {
   // debug('click target el', $(e.target)[0].tagName)
 
   var targetTag = $(e.target)[0].tagName
-  if (targetTag !== 'INPUT' && targetTag !== 'BUTTON' && targetTag !== 'A') {
+  if (!targetTag.match('INPUT|BUTTON|A')) {
     // If it's the last beat
     if (clickX > (pageWidth / 2.5)) {
       // A click on the right side of the window
